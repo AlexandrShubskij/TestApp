cb4951ea76d243c1e0e1e26831d3b2706f560948
alexandrshubskij <alexandrshubskij@gmail.com>
2019-10-14 16:08:24 +0300

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Александр Шубский on 14.10.2019.
+//  Copyright © 2019 Александр Шубский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
